d86371d696d8141e21affe9d7fbe39a8599ea94d
phatnguyen81@gmail.com <Admin@123>
2014-12-25 21:47:29 +0700

    Add Move Product, Login Page

---
+++ QuanLyThietBi/Controllers/WareHouseController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuanLyThietBi.Controllers
+{
+    public class WareHouseController : Controller
+    {
+        // GET: WareHouse
+        public ActionResult MoveProduct()
+        {
+            return View();
+        }
+    }
+}
